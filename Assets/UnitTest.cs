20a9891d2659d17c36b511f69ba942ce65bc008c
ted <ted10401@gmail.com>
2019-02-27 17:19:03 +0800

    Add the unit test.

---
+++ Assets/UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using JSLCore.Pool;
+
+public class UnitTest : MonoBehaviour
+{
+    public GameObject gameObjectReference;
+    private Queue<GameObject> m_recycleGameObjects = new Queue<GameObject>();
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            m_recycleGameObjects.Enqueue(PoolManager.Instance.Get(gameObjectReference));
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2) && m_recycleGameObjects.Count > 0)
+        {
+            PoolManager.Instance.Recycle(m_recycleGameObjects.Dequeue());
+        }
+    }
+}
